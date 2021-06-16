03794be8a5c4398c7b476b4a19becd8167b7c71a
The-beginner-2 <77925257+The-beginner-2@users.noreply.github.com>
2021-06-16 22:05:27 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.diretion_slider').slick({
+        arrows:false,
+        dots:true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        infinite: true,
+    });
+});
